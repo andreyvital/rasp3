3e897e348c57c8204d95dde0a1108082bb87852b
Andrey <andreykvital@gmail.com>
2015-12-22 20:08:57 -0200

    Initial work on HTTP server

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/BurntSushi/toml"
+	"github.com/CentaurWarchief/rasp3/config"
+)
+
 func main() {
+	defer func() {
+		os.Exit(1)
+	}()
+
+	var cfg config.Config
+
+	if _, err := toml.DecodeFile("config.toml", &cfg); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.ListenPort), nil); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
