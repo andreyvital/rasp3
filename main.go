c5e0c53fe532bace50fd82c7a3740a599217f623
Andrey <andreykvital@gmail.com>
2015-12-23 00:02:38 -0200

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/CentaurWarchief/rasp3/config"
+	"github.com/CentaurWarchief/rasp3/gql"
 	"github.com/CentaurWarchief/rasp3/mp3"
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -23,6 +25,7 @@ func main() {
 	}
 
 	library := mp3.NewInMemoryLibrary()
+	gallery := mp3.NewInMemoryArtworkGallery(mp3.Id3ArtworkLoader)
 
 	for _, path := range cfg.Discover {
 		go mp3.Discover(
@@ -31,7 +34,17 @@ func main() {
 		)
 	}
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.ListenPort), nil); err != nil {
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.Methods("GET").
+		Path("/mp3/{id}/artwork").
+		HandlerFunc(mp3.GetArtworkImage(library, gallery))
+
+	r.Methods("POST").
+		Path("/query").
+		HandlerFunc(gql.QueryHandler(library, gallery))
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.ListenPort), r); err != nil {
 		fmt.Println(err.Error())
 		return
 	}
